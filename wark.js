b2a1a08c5fc9052b998356f46f7c2e072efeadc2
k-t-c2025 <tora.c5050@gmail.com>
2025-09-18 23:43:28 +0900

    Add files via upload

--- wark.js
+++ wark.js
@@ -1,2 +1,11 @@
-alert('中山の芝とダートにリンク付けたよ');
+const navItems = document.querySelectorAll('.bottom-nav .nav-item');
+const currentPath = location.pathname.split('/').pop() || 'index.html';
+navItems.forEach(function(link){
+	const href = link.getAttribute('href');
+	if (href === currentPath) {
+		link.classList.add('active');
+	}
+});
+
+console.log('bottom nav initialized');
 
